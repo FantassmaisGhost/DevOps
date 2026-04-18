83f2536a15c18a94d25a83fbf7e44ce468663a18
Missy-Ntontela <2673619@students.wits.ac.za>
2026-04-18 03:55:41 +0200

    Update App.jsx

--- HealthFlow/src/App.jsx
+++ HealthFlow/src/App.jsx
@@ -1,1 +1,18 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import LoginForm from './components/LoginForm';
 
+function App() {
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
