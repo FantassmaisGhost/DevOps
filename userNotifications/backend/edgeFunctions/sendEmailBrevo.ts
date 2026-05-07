3abb4f725f6009789b74a4b1e98ba8c508f7b2b8
Tendai <2672572@students.wits.ac.za>
2026-05-07 15:46:08 +0200

    just seeing if git is correctly tracking my work

--- userNotifications/backend/edgeFunctions/sendEmailBrevo.ts
+++ userNotifications/backend/edgeFunctions/sendEmailBrevo.ts
@@ -3,3 +3,9 @@
 //Please change the file name to index.ts and add it to the edge functions in supabase.
 
 //I just named it as sendEmailBrevo.ts for github distinguishing purposes.
+
+//----------------------------------------------------------------------------------------------------
+
+// supabase/functions/send-email-brevo/index.ts
+
+const BREVO_API_KEY = Deno.env.get("BREVO_API_KEY");
