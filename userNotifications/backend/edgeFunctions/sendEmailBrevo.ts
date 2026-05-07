9dad9d950128dd2aa8b9ad22cd130031c1506137
Tendai <2672572@students.wits.ac.za>
2026-05-07 15:35:09 +0200

    uploading Brevo email edge function

---
+++ userNotifications/backend/edgeFunctions/sendEmailBrevo.ts
@@ -0,0 +1,5 @@
+//BEFORE YOU DO ANYTHING...
+
+//Please change the file name to index.ts and add it to the edge functions in supabase.
+
+//I just named it as sendEmailBrevo.ts for github distinguishing purposes.
