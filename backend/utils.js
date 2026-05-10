72e21de585da9fb98b5ecebaf5386ee14c55ffa9
Lungani Shezi <dyingmountain58@gmail.com>
2026-05-10 12:45:55 +0200

    utils.test.js changes : Director goes to ../backend/Util

--- backend/utils.js
+++ backend/utils.js
@@ -1,9 +1,9 @@
-// utils.js
+// backend/utils.js
 export class Utils {
-  export static DAY_NAMES = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-  export static DAY_SHORT = ['Su','Mo','Tu','We','Th','Fr','Sa'];
-  export static MONTH_NAMES = ['January','February','March','April','May','June','July','August','September','October','November','December'];
-  export static SLOT_MINUTES = 30;
+  static DAY_NAMES = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+  static DAY_SHORT = ['Su','Mo','Tu','We','Th','Fr','Sa'];
+  static MONTH_NAMES = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+  static SLOT_MINUTES = 30;
 
   static generateSlots(opentime, closingtime, intervalMinutes = this.SLOT_MINUTES) {
     if (!opentime || !closingtime) return [];
