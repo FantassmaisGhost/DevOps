5bf9e9be76bdfe3200417d8d2b8b47ddb46ab7d7
MkatekoCass <2706506@students.wits.ac.za>
2026-05-18 13:23:54 +0200

    added and fixed from hellomain

--- backend/supabase.js
+++ backend/supabase.js
@@ -1,5 +1,5 @@
 // supabase.js
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.39.8';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 export const supabaseUrl = 'https://ixikhufrylaugpdxokwu.supabase.co';
 export const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4aWtodWZyeWxhdWdwZHhva3d1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2NTQ0NTIsImV4cCI6MjA5MTIzMDQ1Mn0.F7g_bNWAsxjWtkHihVNYPicghiKOisgHGV9-zaBjXvQ';
